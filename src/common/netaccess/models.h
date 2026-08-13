cbce0b6d3804eee41721439552019cde8177db62
deb <deb@debian.debian.local>
2026-08-13 20:56:30 +0300

    Fix formatting

--- src/common/netaccess/models.h
+++ src/common/netaccess/models.h
@@ -1,8 +1,8 @@
-#include <vector>
-#include <string>
 #include <filesystem>
-#include <variant>
 #include <map>
+#include <string>
+#include <variant>
+#include <vector>
 
 // ABAC (Attribute-Based Access Control)
 namespace models
@@ -12,32 +12,34 @@ namespace fs = std::filesystem;
 using ID = uint64_t;
 using AttributeValue = std::variant<int, std::string, bool, fs::perms>;
 
-template<typename T>
-concept ValidAttributeType = std::same_as<T, int> || 
-                             std::same_as<T, std::string> || 
-                             std::same_as<T, bool> || 
-                             std::same_as<T, fs::perms>;
+template <typename T>
+concept ValidAttributeType =
+    std::same_as<T, int> || std::same_as<T, std::string> || std::same_as<T, bool> || std::same_as<T, fs::perms>;
 
-struct Subject {
+struct Subject
+{
     std::string id;
     std::map<std::string, AttributeValue, std::less<>> attributes;
 
-    template<ValidAttributeType T>
-    [[nodiscard]] T get_attribute(std::string_view key, T default_value) const {
+    template <ValidAttributeType T> [[nodiscard]] T get_attribute(std::string_view key, T default_value) const
+    {
         auto it = attributes.find(key);
-        if (it != attributes.end() && std::holds_alternative<T>(it->second)) {
+        if (it != attributes.end() && std::holds_alternative<T>(it->second))
+        {
             return std::get<T>(it->second);
         }
         return default_value;
     }
 };
 
-struct Resource {
+struct Resource
+{
     std::string name;
-    std::filesystem::perms required_perms; 
+    std::filesystem::perms required_perms;
 };
 
-[[nodiscard]] bool evaluate_policy(const Subject& subject, const Resource& resource) {
+[[nodiscard]] bool evaluate_policy(const Subject& subject, const Resource& resource)
+{
     int clearance = subject.get_attribute<int>("clearance_level", 0);
     if (clearance < 3)
     {
@@ -46,8 +48,7 @@ struct Resource {
 
     auto user_perms = subject.get_attribute<std::filesystem::perms>("allowed_perms", std::filesystem::perms::none);
 
-
     return (user_perms & resource.required_perms) == resource.required_perms;
 }
 
-}
+} // namespace models
